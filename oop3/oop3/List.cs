609d217753fbda7b1df651a73b2caa95fbf1d535
Kristina <kristina.gurina.03@gmail.com>
2022-09-22 01:31:26 +0300

    Lab03 Add List

---
+++ oop3/oop3/List.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace oop3
+{
+    class List
+    {
+        public class Developer
+        {
+            public int id;
+            public string DevName;
+            public string department;
+
+            public Developer(int id, string DevName, string department)
+            {
+                this.id = id;
+                this.DevName = DevName;
+                this.department = department;
+
+            }
+        }
+
+        public List()
+        {
+            this.MyList = new List<int>();
+        }
+
+        public List<int> MyList { get; set; }
+        public int Count => this.MyList.Count;  //количество
+        public void AddItem(int element) => this.MyList.Add(element);  //добавление элемента
+        public int RemoveItem()    //удаление элемента
+        {
+            int lastElementIndex = this.MyList.Count - 1;
+            int lastElement = this.MyList[lastElementIndex];
+            this.MyList.RemoveAt(lastElementIndex);
+            return lastElement;
+        }
+
+        public void RemoveAtPos(int pos) //удаление элемента в заданной позиции
+        {
+            this.MyList.RemoveAt(pos);
+        }
+
+        public void PrintList()
+        {
+            Console.WriteLine("Список: ");
+            for (int i = 0; i < MyList.Count; i++)
+            {
+                Console.WriteLine(MyList[i]);
+            }
+        }
+        //индексатор
+        public int this[int index]
+        {
+            get => this.MyList[index];
+
+            set => this.MyList[index] = value;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ oop3/oop3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace oop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List myList = new List();
+
+            myList.AddItem(9);
+            myList.AddItem(17);
+            myList.AddItem(19);
+            myList.AddItem(21);
+            myList.AddItem(145);
+            myList.AddItem(25);
+
+            myList.PrintList();
+
+            myList.RemoveItem();
+            myList.PrintList();
+
+            myList.RemoveAtPos(3);
+            myList.PrintList();
+            
+            myList.AddItem(1111);
+            myList.PrintList();
+
+
+        }
+    }
+}
