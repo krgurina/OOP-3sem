56ed036d7ed6c65144d56770499ccb92c5fc5afa
Kristina <kristina.gurina.03@gmail.com>
2022-11-13 22:51:41 +0300

    Lab08 Add Game

---
+++ oop8/oop8/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop8
+{
+    class Game
+    {
+        public string name { get; set; }
+        public int hp { get; set; }
+        public int fullHP { get; set; }
+
+        public Game(string Name, int Health)
+        {
+            name = Name;
+            hp = Health;
+            fullHP = Health;
+        }
+
+        public delegate void Actions(Game obj);
+        public event Actions? Attack;
+        public event Actions? Treat;
+
+        //метод, инициирующий событие
+        public void ToAtt(Game obj)   
+        {
+            Console.WriteLine();
+            obj.hp -= 50;
+            Console.WriteLine(this.name + " атаковал "+ obj.name);
+            Attack?.Invoke(obj);//вызов события, если не null
+
+        }
+        public void ToTreat(Game obj)
+        {
+            Console.WriteLine();
+            if (obj.hp < obj.fullHP)
+                obj.hp += 20;
+            Console.WriteLine(this.name + " вылечил " + obj.name);
+            Treat?.Invoke(obj);//вызов события, если не null
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ oop8/oop8/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace oop8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== Game ==========");
+
+            Game user1 = new Game("Steve", 300);
+            Game zombie = new Game("Зомби", 150);
+            Game pig = new Game("Хрюня", 100);
+
+            user1.Attack += User1_Attack;
+            zombie.Attack += Zombie_Attack;
+            zombie.ToAtt(pig);
+            user1.ToAtt(zombie);
+            user1.ToAtt(zombie);
+            user1.ToAtt(zombie);
+            user1.Treat += User1_Treat;
+            user1.ToTreat(pig);
+
+
+
+
+
+
+
+
+        }
+
+        //обработка события
+        private static void User1_Treat(Game obj)
+        {
+            Console.WriteLine($"{obj.name}\t[XP:{obj.hp}/{obj.fullHP}]");
+
+            
+            
+        }
+
+        private static void Zombie_Attack(Game obj)
+        {
+            Console.WriteLine($"{obj.name} заражён, {obj.name} может стать зомби!");
+            if (obj.hp <= 0)
+            {
+                Console.WriteLine($"{obj.name} убит!");
+            }
+            else
+            {
+                Console.WriteLine($"{obj.name}\t[XP:{obj.hp}/{obj.fullHP}]");
+            }
+        }
+
+        private static void User1_Attack(Game obj)
+        {
+            if (obj.hp <= 0)
+            {
+                Console.WriteLine($"{obj.name} убит!");
+            }
+            else
+            {
+                Console.WriteLine($"{obj.name}\t[XP:{obj.hp}/{obj.fullHP}]");
+            }
+            //throw new NotImplementedException();
+        }
+    }
+}
