96a5dfebccaec6c64a5438bf0099a208545fb88c
Kristina <kristina.gurina.03@gmail.com>
2022-11-18 21:33:53 +0300

    Lab09 Add Computer and realize ISet

---
+++ oop9/oop9/Computer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop9
+{
+    class Computer//:ISet<T>
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+
+        public Computer(int id,int price)
+        {
+            Id = id;
+            Price = price;
+        }
+
+
+    }
+}
---
+++ oop9/oop9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
