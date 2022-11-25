82a62857ce071a65a6d65037e443f94e5106ad86
Kristina <kristina.gurina.03@gmail.com>
2022-11-25 16:46:11 +0300

    Lab10 LINQ for books

---
+++ oop10/oop10/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop10
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublishYear { get; set; }
+
+        public int PageNumber { get; set; }
+        public int Price { get; set; }
+
+        public Book(string Title, string Author, int PublishYear, int PageNumber, int Price)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.PublishYear = PublishYear;
+            this.PageNumber = PageNumber;
+            this.Price = Price;
+        }
+        public override string ToString()
+        {
+            return $"{Title}, {Author}, {PublishYear} год, {PageNumber} страниц, ЦЕНА:{Price}";
+        }
+    }
+}
--- oop10/oop10/Program.cs
+++ oop10/oop10/Program.cs
@@ -8,12 +8,13 @@ namespace oop10
     {
         static void Main(string[] args)
         {
+            //1
             string[] months = {"January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December", };
 
             Console.WriteLine("Введите число n");
             int n = Convert.ToInt32(Console.ReadLine());
-            //int n = 4;
+
             var nLengthhMonths = from t in months
                                   where t.Length == n
                                   select t;
@@ -53,6 +54,97 @@ namespace oop10
                 Console.WriteLine(item);
             }
 
+            //2
+            Book book1 = new Book("Война и мир", "Толстой", 1869, 1200, 40);
+            Book book2 = new Book("Бородино","Лермонтов", 1977, 120, 30);
+            Book book3 = new Book("Мастер и Маргарита","Булгаков", 1987, 452, 20);
+            Book book4 = new Book("", "Маяковский", 1995, 180, 150);
+            Book book5 = new Book("","Толстой", 1945, 190, 17);
+            Book book6 = new Book("", "Есенин", 1998, 900, 19);
+            Book book7 = new Book("", "Ахматова", 1988, 160, 14);
+            Book book8 = new Book("", "Пришвин", 1936, 500, 11);
+            Book book9 = new Book("", "Чехов", 1925, 320, 31);
+            Book book10 = new Book("", "Бродский", 1967, 7550, 56);
+            
+            List<Book> list = new List<Book>();
+            list.Add(book1);
+            list.Add(book2);
+            list.Add(book3);
+            list.Add(book4);
+            list.Add(book5);
+            list.Add(book6);
+            list.Add(book7);
+            list.Add(book8);
+            list.Add(book9);
+            list.Add(book10);
+
+            //Console.WriteLine("Введите фамилию автора(Например, Толстой: ");
+            //string author = Console.ReadLine();
+            string author = "Толстой";
+            int year = 1869;
+            //Console.WriteLine("Введите год(Например, 1869: ");
+            //int year = Convert.ToInt32(Console.ReadLine());
+
+            var AuthorBooks = from t in list
+                                where t.Author == author
+                                where t.PublishYear == year
+                                select t;
+
+            var AfretYaerBooks = from t in list
+                                 where t.PublishYear > year
+                                 select t;
+
+            var ThinBooks = from t in list
+                                 orderby t.PageNumber
+                                 select t;
+            var TheThinestBook = ThinBooks.First();
+
+            var BigChipdBooks = from t in list
+                                 orderby t.Price
+                                 orderby t.PageNumber
+                                 select t;
+            var SelectedBooks = BigChipdBooks.TakeLast(5);
+
+            //var PriseList = from t in list
+            //                     orderby t.Price
+            //                     select t;
+
+            // метод расширения
+            var PriseList = list.OrderBy(t => t.Price).Select(t => t);
+
+
+
+
+
+            Console.WriteLine("=====================AuthorBooks=====================");
+            foreach (var item in AuthorBooks)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("=====================AfretYaerBooks=====================");
+            foreach (var item in AfretYaerBooks)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("=====================TheThinestBook=====================");
+            Console.WriteLine(TheThinestBook);
+            Console.WriteLine();
+            Console.WriteLine("=====================SelectedBooks=====================");
+            foreach (var item in SelectedBooks)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("=====================PriseList=====================");
+            foreach (var item in PriseList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
         }
+
     }
 }
