0ecf5a840c3be401293d38f85720aa07e26cbaf4
Kristina <kristina.gurina.03@gmail.com>
2022-12-10 22:55:31 +0300

    Lab12 Add Search and Delete

--- oop12/oop12/GKSLog.cs
+++ oop12/oop12/GKSLog.cs
@@ -11,9 +11,11 @@ namespace oop12
         public static void WriteLogInfo()               /// запись в файл лога инфы о работе самого логгера
         {
             string loGKSth = Path.GetFullPath(@"F:\лабы\ООП\labs\oop12\oop12\gkslog.txt");
-            using (StreamWriter sw = new StreamWriter(loGKSth, false, System.Text.Encoding.Default))
+            using (StreamWriter sw = new StreamWriter(loGKSth, true, System.Text.Encoding.Default))
             {
+                sw.WriteLine("\n================================================================================================\n");
                 sw.WriteLine("\n===========================================   GKSLog   =========================================\n");
+                sw.WriteLine("\n================================================================================================\n");
                 sw.WriteLine("Имя файла лога:           " + Path.GetFileName(loGKSth));
                 sw.WriteLine("Полный путь лога:         " + loGKSth);
                 sw.WriteLine("Время записи лога:        " + DateTime.Now);
@@ -37,12 +39,111 @@ namespace oop12
             return sr.ReadToEnd();
         }
 
+        //Найдите и выведите сохраненную информацию в файле xxxlogfile.txt о действиях пользователя за определенный день/ диапазон времени/по ключевому слову.
+
+        // за определенный день 
+        public static void FindLogInfoDay(DateTime date)
+        {
+            string GKS = Path.GetFullPath(@"F:\лабы\ООП\labs\oop12\oop12\gkslog.txt");
+            string logInfo = ReadLog();
+            string[] logInfoArray = logInfo.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] logInfoArray2 = new string[logInfoArray.Length];
+            int count = 0;
+
+            for (int i = 0; i < logInfoArray.Length; i++)
+            {
+                if (logInfoArray[i].Contains(date.ToString()))
+                {
+                    logInfoArray2[count] = logInfoArray[i];
+                    count++;
+                }
+            }
+
+            for (int i = 0; i < count; i++)
+                Console.WriteLine(logInfoArray2[i]);
+        }
+
+        // по определенному слову
+        public static void FindLogInfo(string keyWord)
+        {
+            string GKS = Path.GetFullPath(@"F:\лабы\ООП\labs\oop12\oop12\gkslog.txt");
+            string logInfo = ReadLog();
+            string[] logInfoArray = logInfo.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] keyWordArray = keyWord.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] logInfoArray2 = new string[logInfoArray.Length];
+            int count = 0;
+
+            for (int i = 0; i < logInfoArray.Length; i++)
+            {
+                for (int j = 0; j < keyWordArray.Length; j++)
+                {
+                    if (logInfoArray[i].Contains(keyWordArray[j]))
+                    {
+                        logInfoArray2[count] = logInfoArray[i];
+                        count++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < count; i++)
+                Console.WriteLine(logInfoArray2[i]);
+
+           
+        }
+
+        // за определенный диапазон 
+        public static void FindLogInfoTime(DateTime date1, DateTime date2)
+        {
+            string GKS = Path.GetFullPath(@"F:\лабы\ООП\labs\oop12\oop12\gkslog.txt");
+            string logInfo = ReadLog();
+            string[] logInfoArray = logInfo.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] logInfoArray2 = new string[logInfoArray.Length];
+            int count = 0;
+
+            for (int i = 0; i < logInfoArray.Length; i++)
+            {
+                if (logInfoArray[i].Contains(date1.ToString()) && logInfoArray[i].Contains(date2.ToString()))
+                {
+                    logInfoArray2[count] = logInfoArray[i];
+                    count++;
+                }
+            }
+
+            for (int i = 0; i < count; i++)
+                Console.WriteLine(logInfoArray2[i]);
+        }
+       
+        //Удалите часть информации, оставьте только записи за текущий час.
+        public static void DeleteLogInfo()
+        {
+            string GKS = Path.GetFullPath(@"F:\лабы\ООП\labs\oop12\oop12\gkslog.txt");
+            string logInfo = ReadLog();
+            string[] logInfoArray = logInfo.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] logInfoArray2 = new string[logInfoArray.Length];
+            int count = 0;
+            DateTime date = DateTime.Now;
+            date.AddHours(-1);
+            for (int i = 0; i < logInfoArray.Length; i++)
+            {
+                if (logInfoArray[i].Contains(date.ToString()))
+                {
+                    logInfoArray2[count] = logInfoArray[i];
+                    count++;
+                }
+            }
+            string logInfo2 = "";
+            for (int i = 0; i < count; i++)
+                logInfo2 += logInfoArray2[i] + "\n";
+            File.WriteAllText(GKS, logInfo2);
+        }
+
+
 
         public static void SearchLog()
         {
-            string loGKSth = Path.GetFullPath(@"F:\лабы\ООП\labs\oop12\oop12\gkslog.txt");
+            string GKS = Path.GetFullPath(@"F:\лабы\ООП\labs\oop12\oop12\gkslog.txt");
             string logFile = GKSLog.ReadLog();                                  /// string с самим логом полностью
-            FileInfo logFileInfo = new FileInfo(loGKSth);
+            FileInfo logFileInfo = new FileInfo(GKS);
             DateTime lastHour = DateTime.Now;
             lastHour.AddHours(-1);                                              /// записи за последний час
 
--- oop12/oop12/Program.cs
+++ oop12/oop12/Program.cs
@@ -23,10 +23,18 @@ namespace oop12
             GKSFileManager.CopyFile(@"F:\лабы\ООП\labs\oop12\oop12\GKSInspect\gksdirinfo.txt", @"F:\лабы\ООП\labs\oop12\oop12\GKSInspect\gksdirinfo_copy.txt");
             GKSFileManager.CopyFiles(@"F:\лабы\ООП", @"F:\лабы\ООП\labs\oop12\oop12\GKSFiles", @"F:\лабы\ООП\labs\oop12\oop12\GKSInspect");
             GKSFileManager.ZipFiles(@"F:\лабы\ООП\labs\oop12\oop12\GKSInspect\GKSFiles", @"F:\лабы\ООП\labs\oop12\oop12\GKSInspect\GKSFiles.zip", @"F:\лабы\ООП\labs\oop12\oop12\ForZip");
-            //GKSLog.Count();
 
             GKSLog.ReadLog();
-            GKSLog.SearchLog();
+           // GKSLog.SearchLog();
+           // GKSLog.DeleteLogInfo();
+
+            //GKSLog.FindLogInfo("GKSDiskInfo");
+            //GKSLog.FindLogInfoDay(new DateTime(2022, 12, 10, 22, 15, 05));
+            GKSLog.FindLogInfoTime(new DateTime(2022, 12, 10, 22, 15, 05), new DateTime(2022, 12, 10, 22, 37, 10));
+
         }
     }
+
+
+
 }
