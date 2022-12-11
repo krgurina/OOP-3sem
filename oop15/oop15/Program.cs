e364a9bb7edfea8ab39b5a49ceeff7cef4eb6105
Kristina <kristina.gurina.03@gmail.com>
2022-12-12 00:58:26 +0300

    Lab15 Use Task and CancellationToken(1-2 task)

---
+++ oop15/oop15/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Collections.Concurrent;
+
+namespace oop15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First();
+            Console.WriteLine();
+
+            Second();
+            Console.WriteLine();
+        }
+
+        static void First()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Stopwatch sw = new Stopwatch();     // точное измерение затраченного времени
+                sw.Start();
+                Task task = new Task(() => MulByVector(10000));
+                task.Start();
+                Console.WriteLine($"ID: {task.Id}, статус: {task.Status}");
+                task.Wait();
+                Console.WriteLine($"ID: {task.Id}, статус: {task.Status}");
+                sw.Stop();
+                Console.WriteLine($"Производительность: {sw.ElapsedMilliseconds}ms");
+                Console.WriteLine();
+            }
+        }
+
+        static void MulByVector(int k, object ob = null)
+        {
+            Random random = new Random();
+            List<int> vector = new List<int>();
+            for (int i = 0; i < k; i++)
+            {
+                vector.Add(random.Next(1, 10));
+            }
+            vector = vector.Select(x => x * 10).ToList();
+        }
+
+        static void Second()
+        {
+            CancellationTokenSource cancellation = new CancellationTokenSource();
+            Task task = Task.Run(() => MulByVector(1000, cancellation), cancellation.Token);
+            try
+            {
+                cancellation.Cancel();
+                task.Wait();
+            }
+            catch (Exception)
+            {
+                if (task.IsCanceled)
+                    Console.WriteLine("Задача отменена");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
